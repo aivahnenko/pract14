9c1ac291b543fb2ace3170b7430b4709be4d63e8
aivahnenko <aivahnenko866@gmail.com>
2026-04-08 14:06:36 +0300

    3.py

--- 3.py
+++ 3.py
@@ -1,6 +1,12 @@
 list1 = input('Введите 1й список: ').split()
 list2 = input('Введите 2й список: ').split()
-original = [[int(x) for x in list1], [int(x) for x in list2]] #собираем вложенный список из 2х обычных
-filtered = [[int(x) for x in lst if int(x) > 0] for lst in [list1, list2]] #собираем вложенный список с элементами где х>0
+list1_int = [int(x) for x in list1] # преобразую строки в числа
+list2_int = [int(x) for x in list2]
+original = [list1_int, list2_int]
+filtered = [] # оставляю только положительные числа
+for lst in [list1, list2]:
+    good = [int(x) for x in lst if int(x) > 0]
+    filtered.append(good)
+
 print(f'Исходный список: {original}')
 print(f'Отфильтрованный список: {filtered}')
