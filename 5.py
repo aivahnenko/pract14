ec1265ee7c906c4e0db397bd0c2486d33dac9c84
aivahnenko <aivahnenko866@gmail.com>
2026-04-08 14:12:56 +0300

    5.py

--- 5.py
+++ 5.py
@@ -1,6 +1,16 @@
 N = int(input('Сколько предметов может унести игрок (целое число) '))
-cost = list(map(int, input('Ценности всех найденных предметов (список целых чисел, разделённых пробелами) ').split()))
-positive = [x for x in cost if x > 0] #оставляем только предметы с ценностью > 0
-positive.sort(reverse=True) #сортируем от самых ценных
-result = sum(positive[:N]) #берем сумму первых N предметов
+text = input('Ценности всех найденных предметов (список целых чисел, разделённых пробелами) ').split()
+cost_str = text
+cost = [] 
+for x in cost_str:
+    cost.append(int(x))
+positive = [] # Оставляю только предметы с положительной ценностью (ценные предметы)
+for x in cost:
+    if x > 0:
+        positive.append(x)
+positive.sort(reverse=True) # Сортирую от самых дорогих к самым дешёвым
+result = 0 # Беру первые N предметов (самые ценные) и суммирую их
+for i in range(N):
+    if i < len(positive):
+        result = result + positive[i]
 print(f'максимально возможную суммарную ценность предметов {result}')
